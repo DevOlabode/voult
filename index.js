895c610b0e798fd4a82dad3c480ad3126fa7d1d6
Samuel Olabode <Solabode499@gmail.com>
2026-01-09 05:16:59 -0700

    FIxed a bug with the flash messages

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.info = req.flash('info');
+    res.locals.currentUser = req.user;
     next();
   });
 
