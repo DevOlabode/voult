38c64fe0733b1ae034d2efe7ddaec0d9523456eb
Samuel Olabode <Solabode499@gmail.com>
2026-01-10 23:43:36 -0700

    Implementing the app feature into the app codebase

--- controllers/web/app.js
+++ controllers/web/app.js
@@ -2,4 +2,8 @@ const App = require('../../models/app');
 
 module.exports.newForm = (req, res)=>{
     res.render('app/new', {title : 'New App'});
-}
+};
+
+module.exports.newApp = (req, res)=>{
+    console.log(req.body)
+};
--- routes/web/app.js
+++ routes/web/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
 const controller = require('../../controllers/web/app');
+
+const catchAsync = require('../../utils/catchAsync');
+
 const {isLoggedIn} = require('../../middleware');
 
 router.use(isLoggedIn);
 
-router.get('/new', controller.newForm)
+router.get('/new', controller.newForm);
+
+router.post('/', catchAsync(controller.newApp));
 
 module.exports = router;
