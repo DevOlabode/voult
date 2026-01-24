636373da7b0d3107c50a16094d42267a69abcc13
Samuel Olabode <Solabode499@gmail.com>
2026-01-24 09:30:53 -0700

    Added refresh token methods to the models

--- models/refreshToken.js
+++ models/refreshToken.js
@@ -1,26 +1,46 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const refreshTokenSchema = new Schema({
+const refreshTokenSchema = new mongoose.Schema(
+  {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: 'Developer',
       required: true,
+      index: true,
     },
+
     tokenHash: {
       type: String,
       required: true,
+      unique: true,
     },
+
+    clientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Client',
+      required: true,
+    },
+
     expiresAt: {
       type: Date,
       required: true,
     },
+
     revokedAt: Date,
     replacedByToken: String,
-  
+
     ipAddress: String,
-    
     userAgent: String,
-  }, { timestamps: true });
-  
-module.exports = mongoose.model("RefreshToken", refreshTokenSchema);
+  },
+  { timestamps: true }
+);
+
+refreshTokenSchema.virtual('isExpired').get(function () {
+  return Date.now() >= this.expiresAt;
+});
+
+refreshTokenSchema.virtual('isActive').get(function () {
+  return !this.revokedAt && !this.isExpired;
+});
+
+module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
