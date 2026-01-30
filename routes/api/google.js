a43ff7fda39088d7b816f857e3e66d6137642da7
Samuel Olabode <Solabode499@gmail.com>
2026-01-29 20:02:48 -0700

    Making the google oauth work perfectly

--- routes/api/google.js
+++ routes/api/google.js
@@ -6,8 +6,11 @@ const controller = require('../../controllers/api/google');
 const requireEndUserAuth = require('../../middleware/requireEndUserAuth');
 const { verifyClient, verifyClientIdOnly } = require('../../middleware/verifyClient');
 
-router.post('/register', verifyClientIdOnly, controller.googleRegister);
+const validateCallbackUrl = require('../../middleware/validateCallbackUrl');
+const { authLimiter } = require('../../middleware/rateLimiters');
 
-router.post('/login', verifyClientIdOnly, controller.googleLogin);
+router.post('/register', verifyClientIdOnly, validateCallbackUrl, authLimiter, controller.googleRegister);
+
+router.post('/login', verifyClientIdOnly, validateCallbackUrl, authLimiter, controller.googleLogin);
 
 module.exports = router;
