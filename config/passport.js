fe4ad8c3abcfbffab29a44c7c9e8927e2f548e88
Samuel Olabode <solabode499@gmail.com>
2026-02-28 19:15:16 -0700

    Refactor Google OAuth strategy to handle existing accounts and link them with Google ID

--- config/passport.js
+++ config/passport.js
@@ -6,6 +6,36 @@ passport.use(Developer.createStrategy());
 passport.serializeUser(Developer.serializeUser());
 passport.deserializeUser(Developer.deserializeUser());
 
+// passport.use(new GoogleStrategy({
+
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: 'http://localhost:3000/auth/google/callback'
+// }, async (accessToken, refreshToken, profile, done) => {
+//   try {
+
+//     const googleId = profile.id;
+//     const email = profile.emails?.[0]?.value;
+//     const avatar = profile.photos?.[0]?.value;
+
+//     let developer = await Developer.findOne({ googleId });
+
+//     if (!developer) {
+//       developer = await Developer.create({
+//         email,
+//         googleId,
+//         avatar,
+//         name: profile.displayName
+//       });
+//     }
+
+//     return done(null, developer);
+
+//   } catch (err) {
+//     return done(err, null);
+//   }
+// }));
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -17,16 +47,31 @@ passport.use(new GoogleStrategy({
     const email = profile.emails?.[0]?.value;
     const avatar = profile.photos?.[0]?.value;
 
+    // 1️⃣ Check if already linked
     let developer = await Developer.findOne({ googleId });
 
-    if (!developer) {
-      developer = await Developer.create({
-        email,
-        googleId,
-        avatar,
-        name: profile.displayName
-      });
+    if (developer) {
+      return done(null, developer);
     }
+
+    // 2️⃣ Check if email exists (link account)
+    developer = await Developer.findOne({ email });
+
+    if (developer) {
+      developer.googleId = googleId;
+      if (!developer.avatar) developer.avatar = avatar;
+      await developer.save();
+
+      return done(null, developer);
+    }
+
+    // 3️⃣ Otherwise create new account
+    developer = await Developer.create({
+      email,
+      googleId,
+      avatar,
+      name: profile.displayName
+    });
 
     return done(null, developer);
 
