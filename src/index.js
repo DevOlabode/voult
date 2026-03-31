72469c0fe44bb52674bb6dd0898f7d479a38a60b
Samuel Olabode <solabode499@gmail.com>
2026-03-31 13:49:44 -0600

    Update session cookie configuration for enhanced security and add HTTPS redirection middleware for production environment

--- src/index.js
+++ src/index.js
@@ -65,6 +65,13 @@ const corsOptions = {
   ]
 };
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
 app.use(cors(corsOptions));
 
 const swaggerUi = require('swagger-ui-express');
